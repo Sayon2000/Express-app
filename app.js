894f7266b330ab16692803a9e1f5482f2ae5c27c
Sayon Dutta <52020690+Sayon2000@users.noreply.github.com>
2023-08-31 03:16:47 +0530

    parsing implemented

--- app.js
+++ app.js
@@ -1,19 +1,40 @@
 const express = require('express')
-
-
+const path = require('path')
+const bodyParser = require('body-parser')
 const app = express();
 
-app.use((req,res,next)=>{
-    console.log("in middleware")
-    next()
+
+app.use(bodyParser.urlencoded({extended : false}))
+
+app.get('/add-product',(req,res,next)=>{
+    console.log("in another middleware")
+    const fileName ='form.html'
+    const options = {
+        root: path.join(__dirname)
+    };
+ 
+    return res.sendFile(fileName, options, function (err) {
+        if (err) {
+            next(err);
+        } 
+
+})
 })
 
-app.use((req,res,next)=>{
+app.post('/product' , (req,res)=>{
+
+    console.log(req.body.title)
+    console.log(req.body.size)
+    return res.redirect('/')
+})
+
+
+app.use('/',(req,res,next)=>{
     console.log("in another middleware")
     res.send('<h1>Hello to node js</h1>')
-    // res.send({key : "value"})
+
 })
 
 
 
-app.listen(3000)
+app.listen(4000)
